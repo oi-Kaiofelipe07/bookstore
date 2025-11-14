e0a1e64675c6ea7376e233e9f582885464973fd7
Kaio Felipe <kaiofelipee12@gmail.com>
2025-11-13 21:18:15 -0300

    docker-compose

--- bookstore/views.py
+++ bookstore/views.py
@@ -2,8 +2,6 @@ from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
 
-import git
-
 
 @csrf_exempt
 def update(request):
@@ -23,5 +21,5 @@ def update(request):
 
 
 def hello_world(request):
-  template = loader.get_template('hello_world.html')
-  return HttpResponse(template.render())
+    template = loader.get_template('hello_world.html')
+    return HttpResponse(template.render())
